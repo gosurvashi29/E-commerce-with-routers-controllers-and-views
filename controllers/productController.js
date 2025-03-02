1f90196b1dc08b6b45a84115ea7201ce8d798c54
gosurvashi29 <urvashigoswami350@gmail.com>
2025-03-02 19:18:37 +0530

    added views folder for html files

---
+++ controllers/productController.js
@@ -0,0 +1,35 @@
+const path=require("path")
+
+const getProducts=(req, res) => {
+  
+    res.sendFile(path.join(__dirname,"../views/product.html"));
+  };
+
+
+
+  const getProductsById=(req, res) => {
+  
+    res.send(`Fetching product with ID: ${req.params.id}`);
+  };
+
+
+
+  const postProducts=(req, res) => {
+    res.send("Adding a new product");
+   };
+
+   const putProducts=(req, res) => {
+    res.send("Put request called.");
+   };
+   
+
+   const deleteProducts=(req, res) => {
+    res.send("Delete request called.");
+   };
+
+  module.exports={
+    getProducts,
+    getProductsById,
+    postProducts,putProducts,
+    deleteProducts
+  }
